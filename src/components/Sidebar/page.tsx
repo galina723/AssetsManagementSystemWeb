bb64468af154af4d2c1854e52646106f0ec72da1
hoangyen <yenhopie28@gmail.com>
2025-05-06 13:23:28 +0700

    feat: sidebar

--- src/components/Sidebar/page.tsx
+++ src/components/Sidebar/page.tsx
@@ -1,4 +1,6 @@
 import React, { FC } from "react";
+import Image from "next/image";
+import MenuIcon from "@mui/icons-material/Menu";
 
 interface Props {
   collapse?: boolean;
@@ -7,10 +9,35 @@ interface Props {
 const Sidebar: FC<Props> = (props) => {
   const { collapse = false } = props;
 
-  console.log(collapse);
-
   return (
-    <div className="app-sidebar" style={{ width: collapse ? 0 : 250 }}></div>
+    <div className="app-sidebar" style={{ width: collapse ? 0 : 250 }}>
+      <a className="app-sidebar__logo" href="#">
+        <Image
+          src="/logos/logo.png"
+          alt={""}
+          width={collapse ? 0 : 200}
+          height={60}
+        />
+      </a>
+      <div className="app-sidebar__menu">
+        <a className="app-sidebar__menu__item" href="#">
+          <MenuIcon />
+          Menu 1
+        </a>
+        <a className="app-sidebar__menu__item" href="#">
+          <MenuIcon />
+          Menu 2
+        </a>
+        <a className="app-sidebar__menu__item" href="#">
+          <MenuIcon />
+          Menu 3
+        </a>
+        <a className="app-sidebar__menu__item" href="#">
+          <MenuIcon />
+          Menu 4
+        </a>
+      </div>
+    </div>
   );
 };
 
